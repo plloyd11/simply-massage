5621df5f6006f018d0834f85ccd7687a697cf006
plloyd11 <plloyd11@gmail.com>
2018-10-31 16:48:44 -0400

    final QA

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,26 @@
-$("header").headroom({
-    "offset": 205,
-    "tolerance": 5,
-    "classes": {
-        "pinned": "slideInDown",
-        "unpinned": "slideOutUp"
+const form = new Vue({
+    el: "#contactForm",
+    data: {
+      errors: [],
+      name: null,
+      email: null,
+      movie: null
+    },
+    methods: {
+      checkForm: function(e) {
+        this.errors = [];
+        if (!this.name) this.errors.push("Name required.");
+        if (!this.email) {
+          this.errors.push("Email required.");
+        } else if (!this.validEmail(this.email)) {
+          this.errors.push("Valid email required.");
+        }
+        if (!this.errors.length) return true;
+        e.preventDefault();
+      },
+      validEmail: function(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      }
     }
-});
-
-// to destroy
-$("header").headroom("destroy");
-
-// Team hover
-(function($) {
-    (function() {
-        $('.team--individual-img').on('mouseenter', '.team-image', function() {
-            var teamName = $(this).data('name');
-            $(this).attr('src', '/img/team/' + teamName + '-fun.jpg');
-        }).on('mouseleave', '.team-image', function() {
-            var teamName = $(this).data('name');
-            $(this).attr('src', '/img/team/' + teamName + '.jpg');
-        });
-    }());
-}(jQuery));
-
-// Mobile Menu
-$('.mobile-menu-btn').on('click', function() {
-    $('.global--main-nav-right').toggleClass('mobile-menu-open');
-});
-
-new WOW().init();
-
-// Homepage Slider
-
-$(function() {
-    $('.slide').slide({
-      isAutoSlide: true,                
-      isHoverStop: false,                
-      isBlurStop: false,                 
-      isShowDots: false,                 
-      isShowArrow: true,                
-      isHoverShowArrow: true,
-      isLoadAllImgs: false,            
-      slideSpeed: 5000,
-      switchSpeed: 500,
-      dotsClass: 'dots',
-      dotActiveClass: 'active',
-      dotsEvent: 'click',
-      arrowClass: 'arrow',
-      arrowLeftClass: 'arrow-left',     
-      arrowRightClass: 'arrow-right'    
-    });
   });
-
-$(function(){
-    $('#timeline').timelinev({
-        containerDivs:    '.timelineCont',
-        dateDiv:         '.date',
-        textDiv:         '.text',
-        dateHtml:        'h2',
-        textHtml:        'p',
-        dateActiveClass: '.active',
-    });
-});
